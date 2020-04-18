503ee6c96e36c6e511911c9cb1a10ca39c203be2
liuboringbook <1287128847@qq.com>
2020-04-18 13:21:03 +0800

    goodlist

--- miniprogram/components/playlist/playlist.js
+++ miniprogram/components/playlist/playlist.js
@@ -4,22 +4,42 @@ Component({
    * 组件的属性列表
    */
   properties: {
-     playlist: {
+    playlist: {
        type: Object
      } 
   },
-
+  observers:{
+    //playCount
+    //1464123.8
+    ['playlist.playCount'](val){
+       this.setData({
+         _count:this._tranNumber(val,2)
+       })
+    }
+  }, 
   /**
    * 组件的初始数据
    */
   data: {
-
+    _count:0
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    _tranNumber(num,point){
+         let numStr =num.toString().split('.')[0]
+         if(numStr.length<6){
+            return numStr  
+         }else if(numStr.length>=6&&numStr.length<=8){
+           let decimal = numStr.substring(numStr.length-4,numStr.length-4+point)
+//substring() 方法用于提取字符串中介于两个指定下标之间的字符
+           return parseFloat(parseInt(num/10000) +'.'+decimal)+'万';
+         }else if(numStr.length>8){
+           let  decimal =numStr.substring(numStr.length-8,numStr.length-8+point)
+           return parseFloat(parseInt(num / 100000000)+'.'+decimal)+'亿'
+         }
+    }   
   }
 })
