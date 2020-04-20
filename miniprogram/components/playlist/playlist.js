54b1140408ca4dbdf46c933c7c77cede749740e2
liuboringbook <1287128847@qq.com>
2020-04-20 18:36:42 +0800

    musiclist

--- miniprogram/components/playlist/playlist.js
+++ miniprogram/components/playlist/playlist.js
@@ -40,6 +40,11 @@ Component({
            let  decimal =numStr.substring(numStr.length-8,numStr.length-8+point)
            return parseFloat(parseInt(num / 100000000)+'.'+decimal)+'亿'
          }
+    },
+    goToMusiclist(e){
+      wx.navigateTo({
+        url: `../../pages/musiclist/musiclist?playlistId=${this.data.playlist.id}`,
+      })
     }   
   }
 })
